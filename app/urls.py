f1e47864987a716fa457de47ee59f135026502c9
vortex-ninja <vortex.ninja@protonmail.com>
2018-03-17 23:57:39 +0100

    feat: Add logged in user view

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('users/', views.UserListView.as_view(), name='users'),
+    path('users/me/', views.LoggedInUserView.as_view(), name='logged_in_user'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserListView(generics.ListAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class LoggedInUserView(APIView):
+
+    def get(self, request, format=None):
+        if isinstance(request.user, get_user_model()):
+            serializer = UserSerializer(request.user)
+            return Response(serializer.data)
+        else:
+            return Response({"error": "no user is logged in."})
