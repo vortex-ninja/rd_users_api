a46d8ca14ca2c1386e4f37f231da0bb5ceadc27a
vortex-ninja <vortex.ninja@protonmail.com>
2018-03-18 00:27:14 +0100

    feat: Add create user view

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('users/', views.UserListView.as_view(), name='users'),
     path('users/me/', views.LoggedInUserView.as_view(), name='logged_in_user'),
     path('login/', obtain_jwt_token),
+    path('register/', views.CreateUserView.as_view(), name='create_user'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- app/views.py
+++ app/views.py
@@ -2,8 +2,11 @@ from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 from .serializers import UserSerializer
+from .tokens import jwt_payload_handler, jwt_encode_handler
 
 
 class UserListView(generics.ListAPIView):
@@ -19,3 +22,31 @@ class LoggedInUserView(APIView):
             return Response(serializer.data)
         else:
             return Response({"error": "no user is logged in."})
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    # I add the token when user creation was succesful and response is ready
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        # Get the created user instance
+
+        created_user = get_user_model().objects.get(pk=serializer.data['id'])
+
+        # Create token for newly created user
+
+        payload = jwt_payload_handler(created_user)
+        token = jwt_encode_handler(payload)
+
+        # Include token in the response
+
+        response = {"created_user": serializer.data, "token" : token}
+
+        return Response(response, status=status.HTTP_201_CREATED, headers=headers)
