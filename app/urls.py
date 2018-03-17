ee0661349beea3642fe8f3e5850dfc02028d714a
vortex-ninja <vortex.ninja@protonmail.com>
2018-03-18 00:13:12 +0100

    feat: Add JWT token support and login view

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 from . import views
 
 urlpatterns = [
     path('users/', views.UserListView.as_view(), name='users'),
     path('users/me/', views.LoggedInUserView.as_view(), name='logged_in_user'),
+    path('login/', obtain_jwt_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
